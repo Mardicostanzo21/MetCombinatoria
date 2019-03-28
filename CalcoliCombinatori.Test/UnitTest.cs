65d57ae8761590d708547c4a5df17ebda8c422ff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-28 09:17:05 +0100

    Completato

---
+++ CalcoliCombinatori.Test/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcoliCombinatori.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [DataTestMethod]
+        [DataRow(0,1)]
+        [DataRow(1, 1)]
+        [DataRow(5, 120)]
+        [DataRow(10, 3628800)]
+        [DataRow(50,0)]
+
+
+        public void TestMethod1(int N1, int N2)
+        {
+            int Fattoriale;
+
+            Fattoriale = Convert.ToInt32(equazioniLibrary.CalcoliCombinatori.Fattoriale(N1));
+
+            Assert.AreEqual(Fattoriale, N2);
+        }
+    }
+}
---
+++ equazioniLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equazioniLibrary
+{
+    public class CalcoliCombinatori
+    {
+        public static long Fattoriale(long numero)
+        {
+            int risultato = 1;
+
+            if (numero > 20)
+            {
+                risultato = 0;
+            }
+            else
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    risultato = risultato * i;
+                }
+            }
+            return risultato;
+        }
+
+
+
+
+
+
+    }
+}
